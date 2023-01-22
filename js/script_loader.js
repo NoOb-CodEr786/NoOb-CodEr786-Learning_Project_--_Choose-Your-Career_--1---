44b83cc45cac549318310ccc379900067a1380c4
Surya_Pratap <111687485+NoOb-CodEr786@users.noreply.github.com>
2023-01-22 09:55:10 +0530

    Add files via upload

---
+++ js/script_loader.js
@@ -0,0 +1,7 @@
+var loader = document.querySelector(".loader")
+        window.addEventListener("load",getOut);
+    
+        function getOut()
+        {
+            loader.classList.add("disappear")
+        }
